68f82f8ab657c880e6219a75348c01c5f87a9024
KVeramendi <1611619@utp.edu.pe>
2022-08-29 13:15:54 -0500

    Se agregó la pantalla del Login

---
+++ lib/View/Login/login_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final String _imageRoute = 'assets/images/logo.png';
+  final String _loginButton = 'Iniciar sesión';
+  final String _textDescription = '¿Aún no tienes una cuenta?';
+  final String _registerButton = 'Registrarse';
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 30)),
+                  SizedBox(
+                    height: 200.0,
+                    child: Image.asset(_imageRoute),
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 30)),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: 'Correo electrónico',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 10)),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      labelText: 'Contraseña',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                        child: Icon(
+                          Icons.visibility,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 45)),
+                  Container(
+                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                    width: double.infinity,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: MaterialButton(
+                      onPressed: () {},
+                      color: Colors.greenAccent[700],
+                      child: Text(
+                        _loginButton,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.symmetric(vertical: 5)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        _textDescription,
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                          fontSize: 16.0,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //       builder: (context) => const RegisterPage(),
+                          //     ));
+                        },
+                        child: Text(_registerButton,
+                            style: TextStyle(color: Colors.greenAccent[700])),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
